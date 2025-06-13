4d0146a1af51ebb56db2843737e93eee77b1c24c
Alfonso Gallego <agallego@miptecnologia.local>
2025-06-13 12:15:40 +0200

    added new spanish written versions to process data from jsons and optimize a worker assignment

--- parameter_tuning.py
+++ parameter_tuning.py
@@ -106,7 +106,9 @@ def run_optimization(n_trials: int = 50) -> dict[str, Any]:
     print("Mejor pérdida: ", study.best_value)
     return study.best_params
 
-run_optimization()
+run_optimization(100)
 
 # Primer test:
 # {'capacidad_base': 82, 'capacidad_decaimiento': 7, 'maximo_bonus_especialidad': 30, 'bonus_maximo_jornada': 33, 'penalizacion_no_voluntario_noche': 19}
+# Mejor pérdida: 6810.0
+
